a57784440fd1e65a0314d4619935802f259cc5c3
Alexmerser54 <alexmerser541@gmail.com>
2023-01-15 17:07:56 +0700

    First

---
+++ Laba6/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Laba6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(159, 157);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Предложение без чисел. Предложение 2 с однозначным  числом. Предложение  с двузна" +
+    "чным число 43. Предложение с трёхзначным 881 числом.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вариант 4.1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Вариант 4.2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(193, 13);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(159, 157);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "Тут перечисляются слова, отличные от последнего слова";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(369, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Вариант 7.1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(369, 13);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(159, 157);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "Данный текст состоит из 4 слов и, 372 букв";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(547, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Вариант 7.1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(547, 13);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(159, 157);
+            this.richTextBox4.TabIndex = 6;
+            this.richTextBox4.Text = "Домен www.mailru.group.ru считается верным, а домен www.vk.ru - неверным, как и д" +
+    "омен www.vk.as.rus. Домен www.youtube.support.ru тоже будет верным\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 250);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+    }
+}
+
---
+++ Laba6/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Laba6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Regex regex = new Regex(@"(?<!\d)(\d{2})(?!\d)"); // слева не должно быть цифры, затем идут две цифры, после не должно быть цифры
+            string result = ""; // результирующая строка
+            string text = richTextBox1.Text; // входной текст
+            string[] lines = text.Split(new string[] { ". " }, StringSplitOptions.RemoveEmptyEntries); // разбить строку на предложения с удаленим пустых элементов
+            
+            for (int i = 0; i < lines.Length; i++) // перебор предложений
+            {
+                if (regex.IsMatch(lines[i])) // если в предложении есть совпададение с шаблоном
+                {
+                    result += lines[i] + "\n"; // добавить предложение в результат и конактенировать символ новой строки
+                }
+            }
+            richTextBox1.Text = result; // вывести результат
+        }
+
+        bool HaveRepeaingChars(string inp) // метод для проверки наличия повторяющихся символов
+        {
+            for (int i = 0; i < inp.Length; i++)
+            {
+                 for (int j = i + 1; j < inp.Length; j++) // алгоритм сортировки пузырька
+                {
+                    if (inp[i] == inp[j]) return true; // если буквы равны, то вернуть истину
+                }
+                
+            }
+            return false; // если метод не вернул значение в цикле, то в конце вернуть ложь
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string lastWord; // строка для последнего слова
+            string tmp; // временная переменная
+            string result = ""; // результирующая строка
+            Regex regex = new Regex(@"\S+(?<![.,!:])"); // множество непробельных символов (букв), за исключением знаков препинания
+            string text = richTextBox2.Text; // входной текст
+            var words = regex.Matches(text); // разбить строку на предложения с удаленим пустых элементов
+            lastWord = words[words.Count - 1].ToString().ToLower(); // последнее слово - это последний элемент массива words, 
+                                                                    // приведённый к строке и нижнему регистру
+            for (int i = 0; i < words.Count-1; i++) // перебор слов, не включая последнего
+            {
+                tmp = words[i].ToString(); // присваимваем временной переменной слово
+                if (tmp.ToLower() != lastWord && !HaveRepeaingChars(tmp.ToLower())) // если слово в нижнем регистре не равно последнему слову
+                                                                                    // и в слове в нижнем регистре нет повторяющихся символов
+                    result += tmp + " "; // добавить слово в результат вместе с пробелом
+            }
+
+            richTextBox2.Text = result; // вывести результат
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int wordsCount; // кол-во слов
+            int numsCount; // кол-во чисел
+            string output; // результирующая строка
+            string text = richTextBox3.Text; // входной текст
+
+            Regex wordRegex = new Regex(@"\S+(?<![,.!:]|\d)"); // поиск любых непробельных символов, за исключением знаков препинания и чисел
+
+            Regex numRegex = new Regex(@"\d+"); // поиск цифры, кол-во которых больше или равно 1
+            wordsCount = wordRegex.Matches(text).Count; // количество совпаднений шаблона поиска слов записывается в переменную
+            numsCount = numRegex.Matches(text).Count; // количество совпадений шаблона поиска чисел записывается в переменную
+
+            output = $"В тексте {wordsCount} групп букв и {numsCount} групп цифр\n"; // запись количества в результат
+            if (wordsCount > numsCount) // сравнение
+                output += "Верно, групп букв больше групп цифр"; // запись в результат
+            else output += "Неверно, групп букв не больше групп цифр"; // запись в результат
+
+            richTextBox3.Text = output; // вывод результата
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox4.Text; // входной текст
+            string output = ""; // результирующая строка
+            Regex regex = new Regex(@"www.\w{3,}.\w{3,}.ru"); // шаблон поиска домена. \w{3,} - это минимум 3 буквы
+            foreach (var domen in regex.Matches(text)) // перебор найденных доменов
+            {
+                output += domen.ToString() + " "; // запись найденного домена в результат
+            }
+            richTextBox4.Text = output; // вывод результата
+        }
+    }
+}
